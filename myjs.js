b7426a21feaa71036f19ffe7a3fb29839f6ca171
Alex Souza <59103542+supportalex@users.noreply.github.com>
2022-07-14 01:01:59 -0300

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,12 +1,9 @@
 function tapInProduction() {
-    marcca = prompt('Qual a marca do seu aparelho?')
-    modelo = prompt('Qual o modelo?')
-    Defeito = prompt('Qual o defeito?')
+    marcca = prompt('Página em desenvolvimento! Envie uma sugestão:')
 
-    var Relatotio = "Marca: "  + marcca  + "  Modelo: " + modelo+ "  Defeito: " + Defeito + '.'
-
-    if (window.confirm(Relatotio + '  Os dados estão corretos?')) {
-        window.open('https://api.whatsapp.com/send?phone=5562992936607&text='+ Relatotio);
+    if (window.confirm(marcca)) {
+        window.open('https://api.whatsapp.com/send?phone=5562992936607&text='+ marcca);
       }
 
 }
+
